622831cef4f4e65c2207f104a5cd1eb97602b6d4
Louis Fanien <louis.fanien@gmail.com>
2017-08-14 17:42:50 +0200

    added the friends and users controllers with their empty methods

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,18 @@
+class FriendsController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'friends/show'
+
+  get 'friends/new'
+
+  get 'users/show'
+
+  get 'users/update'
+
+  get 'users/new'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
