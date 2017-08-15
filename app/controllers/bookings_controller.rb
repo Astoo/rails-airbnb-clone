82e014b3cddb05ceddef879fa1c0221a70997a47
Louis Fanien <louis.fanien@gmail.com>
2017-08-15 10:36:12 +0200

    #42 implemented the Show Method in the booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,6 +2,7 @@ class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :update, :destroy]
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def delete
