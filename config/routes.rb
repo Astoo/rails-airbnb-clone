0bc5b08c636b8670cb59dd4dd9c1d67588b23a75
Louis Fanien <louis.fanien@gmail.com>
2017-08-15 10:05:17 +0200

    #59 implemented the resource for user to test the methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,5 @@
 Rails.application.routes.draw do
-  get 'friends/show'
-
-  get 'friends/new'
-
-  get 'users/show'
-
-  get 'users/update'
-
-  get 'users/new'
-
-  get 'bookings/show'
-  
-  get 'reviews/show'
-
-  get 'reviews/new'
-
+  resources :users
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
