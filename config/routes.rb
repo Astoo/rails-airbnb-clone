90a97ad4ff9e75d60dc850455f68a4b62fbd065a
venance valeriane <valeriane.venance@hotmail.fr>
2017-08-15 10:26:18 +0200

    nested routes for reviews inside friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :bookings
   resources :users
-  resources :friends
-  resources :reviews
+  resources :friends do
+    resources :reviews
+  end
+
   devise_for :users
-  root to: 'pages#home'
+  root to: 'friends#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
