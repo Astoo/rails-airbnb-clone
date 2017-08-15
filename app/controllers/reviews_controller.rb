18cd91b5fec065b3be2cb875e99c70de239ee5f0
venance valeriane <valeriane.venance@hotmail.fr>
2017-08-15 11:23:21 +0200

    #43 implemented new method, fixed create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,10 +2,13 @@ class ReviewsController < ApplicationController
   before_action :set_review, only: [:new, :create]
 
   def new
+    @review = Review.new
   end
+  #take care of rending the new and create methods accessible only if a user is logged in.
 
   def create
     @review = Review.new(review_params)
+    @review.user = current_user
     @review.friend = @friend
     if @review.save
       redirect_to friend_path(@friend)
@@ -18,5 +21,9 @@ class ReviewsController < ApplicationController
 
   def set_review
     @friend = Friend.find(params[:friend_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
   end
 end
