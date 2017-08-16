e2916557aa5d0469502c389b08b982f40cef7fec
venance valeriane <valeriane.venance@hotmail.fr>
2017-08-16 16:52:56 +0200

    branch name explicit enough

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -2,6 +2,10 @@ class Friend < ApplicationRecord
   belongs_to :user
   has_many :activitys
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :name, presence: true
+
   has_attachment :avatar
   has_attachments :pictures, maximum: 5
 end
