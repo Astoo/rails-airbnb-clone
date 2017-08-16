bc06ff72895eea90da285e1ec075301fc38dda1c
venance valeriane <valeriane.venance@hotmail.fr>
2017-08-16 18:09:04 +0200

    allowed the unsigned user to see all friends and friends show. unauthorized to comment on the sow

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,7 @@
 class FriendsController < ApplicationController
-
   before_action :set_friend, only: [:show, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     if params["search"] != nil
       city = params["search"]["city"]
