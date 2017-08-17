386ae7e9943600961a440bc28df4850c9aff730b
Louis Fanien <louis.fanien@gmail.com>
2017-08-17 15:01:05 +0200

    #131 added the map and still doing the auto-complete v2

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,9 +1,19 @@
 class Friend < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode, if: :address_changed?
   belongs_to :user
   has_many :activitys
-
   has_attachment :avatar
   has_attachments :pictures, maximum: 5
+
+  geocoded_by :full_address
+  after_validation :geocode, if: :full_address_changed?
+
+  def full_address
+    "#{address}, #{zip_code} #{city} #{ISO3166::Country[country].name}"
+  end
+
+  def full_address_changed?
+    address_changed? || zip_code_changed? || city_changed? || country_changed?
+  end
+
+
 end
