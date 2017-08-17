ca5cd79cbf7b4f34c53b9125fab603120483061e
venance valeriane <valeriane.venance@hotmail.fr>
2017-08-17 14:55:25 +0200

    added name and avatar to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,12 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :bookings
   has_many :friends
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_attachment :avatar
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
